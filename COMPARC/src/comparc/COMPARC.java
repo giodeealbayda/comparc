1e1d3e7fdd58a5cdad75a62f14063ab24e8e6ab2
giodeealbayda <giodeelyn_albayda@dlsu.edu.ph>
2015-03-15 00:08:47 +0800

    Opcode ddiv:

--- COMPARC/src/comparc/COMPARC.java
+++ COMPARC/src/comparc/COMPARC.java
@@ -17,32 +17,33 @@ public class COMPARC {
         MainFrame mf = new MainFrame();
         mf.setVisible(true);
 
-        String instruction = "DSUBU";
+        String instruction = "DDIV";
         String rdString = "R1";
         String rsString = "R2";
         String rtString = "R3";
 
-        int rd = Integer.valueOf(rdString.substring(1, rdString.length()));
-        int rt = Integer.valueOf(rtString.substring(1, rsString.length()));
-        int rs = Integer.valueOf(rsString.substring(1, rsString.length()));
-
-        int opcode;
+        int rd;
+        int rt;
+        int rs;
         int imm;
+        int opcode;
         byte[] instructionByte = new byte[32];
+        int j;
 
-        System.out.println(instruction + " " + rd + ", " + rs + ", " + rt);
         switch (instruction) {
 
             case "DSUBU":
+                rd = Integer.valueOf(rdString.substring(1, rdString.length()));
+                rt = Integer.valueOf(rtString.substring(1, rsString.length()));
+                rs = Integer.valueOf(rsString.substring(1, rsString.length()));
                 opcode = 0;
                 imm = 47;
-                int j;
 
-                // for opcode (6)
+                // opcode 0...5
                 for (j = 5; j >= 0; j--) {
                     instructionByte[j] = 0;
                 }
-                // for rs (5)
+                // for rs (5) 6...10
                 for (j = 10; j >= 6; j--) {
                     if (rs != 0) {
                         if (rs % 2 == 1) {
@@ -56,7 +57,7 @@ public class COMPARC {
                     }
                 }
 
-                // for rt (5)
+                // for rt (5) 11...15
                 for (j = 15; j >= 11; j--) {
                     if (rt != 0) {
                         if (rt % 2 == 1) {
@@ -70,7 +71,7 @@ public class COMPARC {
                     }
                 }
 
-                // for rd
+                // for rd 16...20
                 for (j = 20; j >= 16; j--) {
                     if (rd != 0) {
                         if (rd % 2 == 1) {
@@ -84,12 +85,12 @@ public class COMPARC {
                     }
                 }
 
-                // another 5 (5)
+                // another 5 (5) 21...25
                 for (j = 25; j >= 21; j--) {
                     instructionByte[j] = 0;
                 }
 
-                // func (6)
+                // func (6) 26...31
                 for (j = 31; j >= 26; j--) {
                     if (imm != 0) {
                         if (imm % 2 == 1) {
@@ -104,6 +105,105 @@ public class COMPARC {
                 }
                 break;
 
+            case "DDIV":
+                opcode = 0;
+                rd = 0;
+                rt = Integer.valueOf(rtString.substring(1, rsString.length()));
+                rs = Integer.valueOf(rsString.substring(1, rsString.length()));
+                imm = 30;
+
+                // opcode 0...5
+                for (j = 5; j >= 0; j--) {
+                    instructionByte[j] = 0;
+                }
+                // for rs (5) 6...10
+                for (j = 10; j >= 6; j--) {
+                    if (rs != 0) {
+                        if (rs % 2 == 1) {
+                            instructionByte[j] = 1;
+                        } else {
+                            instructionByte[j] = 0;
+                        }
+                        rs /= 2;
+                    } else {
+                        instructionByte[j] = 0;
+                    }
+                }
+
+                // for rt (5) 11...15
+                for (j = 15; j >= 11; j--) {
+                    if (rt != 0) {
+                        if (rt % 2 == 1) {
+                            instructionByte[j] = 1;
+                        } else {
+                            instructionByte[j] = 0;
+                        }
+                        rt /= 2;
+                    } else {
+                        instructionByte[j] = 0;
+                    }
+                }
+
+                // for rd 16...20
+                for (j = 20; j >= 16; j--) {
+                    if (rd != 0) {
+                        if (rd % 2 == 1) {
+                            instructionByte[j] = 1;
+                        } else {
+                            instructionByte[j] = 0;
+                        }
+                        rd /= 2;
+                    } else {
+                        instructionByte[j] = 0;
+                    }
+                }
+
+                // another 5 (5) 21...25
+                for (j = 25; j >= 21; j--) {
+                    instructionByte[j] = 0;
+                }
+
+                // func (6) 26...31
+                for (j = 31; j >= 26; j--) {
+                    if (imm != 0) {
+                        if (imm % 2 == 1) {
+                            instructionByte[j] = 1;
+                        } else {
+                            instructionByte[j] = 0;
+                        }
+                        imm /= 2;
+                    } else {
+                        instructionByte[j] = 0;
+                    }
+                }
+                break;
+
+            case "AND":
+                break;
+
+            case "DSRLV":
+                break;
+
+            case "SLT":
+                break;
+
+            case "BEQ":
+                break;
+
+            case "LW":
+                break;
+
+            case "LWU":
+                break;
+
+            case "SW":
+                break;
+
+            case "DADDIU":
+                break;
+
+            case "J":
+                break;
             default:
                 System.out.println("ERROR");
                 break;
